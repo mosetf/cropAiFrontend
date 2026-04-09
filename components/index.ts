f0387b7e18344cf71fe1d857293dc3ed4d06d0a4
Mosetf <stanlym315@gmail.com>
2026-04-10 02:09:33 +0300

    Add components/index.ts

---
+++ components/index.ts
@@ -0,0 +1,14 @@
+// UI Components
+export { default as Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { default as Card } from './Card';
+export type { CardProps } from './Card';
+
+export { default as StatCard } from './StatCard';
+export type { StatCardProps } from './StatCard';
+
+export { default as Navbar } from './Navbar';
+
+export { default as PageHeader } from './PageHeader';
+export type { PageHeaderProps } from './PageHeader';
